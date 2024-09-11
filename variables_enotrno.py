77ff36f5870d29cfe32f8871b1a4fee3c51774b2
LuisMiguel1238907 <133611002+LuisMiguel1238907@users.noreply.github.com>
2024-09-11 07:46:02 -0500

    Update variables_enotrno.py

--- variables_enotrno.py
+++ variables_enotrno.py
@@ -4,23 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Acceder a las variables de entorno definidas en el venv
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
 db_name = os.getenv('DB_NAME')
 
-# Depuración: Verifica que las variables de entorno se carguen correctamente
 print(f"Usuario: {db_user}")
 print(f"Contraseña: {db_password}")
 print(f"Host: {db_host}")
 print(f"Base de datos: {db_name}")
 
-# Verificar si las variables están cargadas
 if not all([db_user, db_password, db_host, db_name]):
     print("Error: Algunas variables de entorno no se han cargado correctamente.")
 else:
-    # Establecer la conexión con la base de datos
+ 
     try:
         connection = mysql.connector.connect(
             host=db_host,
@@ -29,7 +26,7 @@ else:
             database=db_name
         )
 
-        # Verificar si la conexión fue exitosa
+      
         if connection.is_connected():
             db_info = connection.get_server_info()
             print(f"Conectado a MySQL Server versión {db_info}")
