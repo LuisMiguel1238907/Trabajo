091e147cfd8a8a18856c0f09b2b9ffa1b4948f53
LuisMiguel1238907 <133611002+LuisMiguel1238907@users.noreply.github.com>
2024-09-11 07:48:06 -0500

    Update combinacion.py

--- combinacion.py
+++ combinacion.py
@@ -1,14 +1,10 @@
 import pandas as pd
 
-# Cargar los archivos de Excel en DataFrames
 df_pacientes = pd.read_excel('D:/UNIVERSIDAD/6 semestre/Bases de datos/Pacientes.xlsx')
 df_tratamientos = pd.read_excel('D:/UNIVERSIDAD/6 semestre/Bases de datos/Tratamientos.xlsx')
 
-# Combinar ambos DataFrames utilizando la columna 'ID_Paciente' como clave común
 df_combinado = pd.merge(df_pacientes, df_tratamientos, on='ID_Paciente')
 
-# Guardar el DataFrame combinado en un nuevo archivo Excel
 df_combinado.to_excel('Datos_Combinados.xlsx', index=False)
 
-# Opcional: Mostrar las primeras filas del DataFrame combinado
 print(df_combinado.head())
